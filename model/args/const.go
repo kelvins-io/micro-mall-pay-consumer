ae68eaf970be19357afcf4188e16f7f5da671df3
kelvins-io <mybaishati@gmail.com>
2021-10-03 07:48:37 +0800

    搜索同步

--- model/args/const.go
+++ model/args/const.go
@@ -27,10 +27,10 @@ const (
 )
 
 type CommonBusinessMsg struct {
-	Type int    `json:"type"`
-	Tag  string `json:"tag"`
-	UUID string `json:"uuid"`
-	Msg  string `json:"msg"`
+	Type    int    `json:"type"`
+	Tag     string `json:"tag"`
+	UUID    string `json:"uuid"`
+	Content string `json:"content"`
 }
 
 type TradePayNotice struct {
